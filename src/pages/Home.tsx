8396fa27df1f7357b8444a1af217ee13e5b55b5a
Dylan Cano Álvarez <dcanalv@upv.edu.es>
2023-01-25 01:40:21 +0100

    added new distribution in Home & pokemon images

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,31 +1,47 @@
 import React, { useEffect, useState } from "react";
 import NavBar from "../components/NavBar";
-import Pokemon from "../components/Pokemon";
-import {getPokemon} from "../services/getData"
+import { getAllPokemon, getPokemon } from "../services/getData"
+import "../pages/Home.css"
+
 
 export default function Home() {
-  const[datos, setDatos]:any = useState();
+  const API_PICTURE = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/"
+  const [listaPokemon, setListaPokemon]: any = useState();
+  const [pokemon, setPokemon]: any = useState();
   useEffect(() => {
     getData()
-  },[])
+  }, [])
 
   async function getData() {
-    const data = await getPokemon()
-    console.log(data)
-    setDatos(data)
+    const data = await getAllPokemon()
+    setListaPokemon(data)
   }
+  async function clickPokemon(url: string) {
+    const data = await getPokemon(url)
+    setPokemon(data)
+  }
+
   return (
     <>
       <NavBar />
-      {
-        datos 
-        ? datos.results.map((pokemon:any, index:any) => {
-          return <p key={index}>{pokemon.name}</p>
-        })
-      : null
-      }
-
-
+      <div className="container text-center ">
+        <div className="row">
+          {
+            listaPokemon
+              ? listaPokemon.results.map((pokemon: any, index: any) => {
+                let div = pokemon.url.split("/");
+                let id_pokedex = div[div.length - 2]
+                return (
+                  <div key={index} className="col-2 card my-1 mx-2" >
+                    <img className="img-size" src={API_PICTURE + id_pokedex + ".png"} alt={pokemon.name}></img>
+                    <button type="button" className="btn btn-info btn-sm" onClick={e => clickPokemon(pokemon.url)}>{pokemon.name + " #" + id_pokedex} </button>
+                  </div>
+                )
+              })
+              : null
+          }
+        </div>
+      </div>
     </>
   );
 }
--- src/services/getData.ts
+++ src/services/getData.ts
@@ -1,8 +1,19 @@
 import axios from "axios";
 const API_URL = "https://pokeapi.co/api/v2/"
 
-export const getPokemon = async () =>{
+export const getAllPokemon = async () =>{
   return axios.get(`${API_URL}pokemon/`)
+    .then(response => {
+      return response.data
+    }
+    )
+    .catch(error => {
+      console.log(error)
+    })
+}
+
+export const getPokemon = async (url:string) => {
+  return axios.get(url)
     .then(response => {
       return response.data
     }
